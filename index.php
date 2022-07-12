4b019c4d264a9c4ecf1ab01f4379972873d9ba63
irineussf <vedder.1@gmail.com>
2022-07-12 11:32:11 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// -- Carrega um usuário -- //
+//$root = new Usuario();
+//$root->loadById(4);,
 
-$root->loadById(4);
+// -- Carrega lista de usuarios -- //
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+// -- Carrega lista de ususarios buscando pelo login -- //
+// $search = Usuario::search("r");
+// echo json_encode($search);
+
+// -- Carrega um ususario usando login e senha -- //
+$ususario = new Usuario();
+$ususario->login("josiane", "456789");
+
+echo $ususario;
